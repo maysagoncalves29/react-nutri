b706c85a27cff9115eb172b63ff7a5e2228bb214
Maysa Gonçalves <maysagoncalves2003@gmail.com>
2022-07-19 17:17:39 -0300

    ReactJS

--- src/App.js
+++ src/App.js
@@ -1,56 +1,53 @@
-import { useState } from 'react';
+import { useState,  useEffect} from 'react';
 
 function App () {
-  const [nome, setNome] = useState('');
-  const [email, setEmail] = useState ('');
-  const [idade, setIdade] = useState(0);
+  const [input, setInput] = useState ('');
+  const [tarefas, setTarefas] = useState([
+    'Pagar contas',
+    'Estudar JS',
+  ]);
 
-  const [user, setUser] = useState({});
+  useEffect(()=> {
+    const tarefasStorage = localStorage.getItem('@tarefa');
+    if(tarefasStorage) {
+      setTarefas(JSON.parse(tarefasStorage))
+    }
+
+  }, []);
+  
+  useEffect(() => {
+    localStorage.setItem('@tarefa', JSON.stringify(tarefas))
+  }, [tarefas]);
 
   function handleRegister(e) {
     e.preventDefault();
-    alert('Usuário registrado com sucesso')
-    setUser({
-      nome: nome,
-      idade:idade,
-      email:email,
-    })
+
+    setTarefas([...tarefas, input]);
+    setInput('');
   }
 
   return (
     <div>
-      <h1>Cadastrando usuário</h1>
+      <h1>Lista de Tarefas</h1>
       <form onSubmit={handleRegister}>
         <label>Nome: </label><br/>
         <input 
         placeholder="Digite seu nome"
-        value={nome} // valor dentro do input (já do html)
-        onChange={(e) => setNome(e.target.value)} //ele chama essa função toda vez que digitar qualquer letra. chama a funcao p/ pegar o que digitou  e coloca dnetro do useState
+        value={input} // valor dentro do input (já do html)
+        onChange={(e) => setInput(e.target.value)} //ele chama essa função toda vez que digitar qualquer letra. chama a funcao p/ pegar o que digitou  e coloca dnetro do useState
         
         /><br/>
 
-        <label>Email: </label><br/>
-        <input 
-        placeholder="Digite seu email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        /><br/>
-
-        <label>Idade: </label><br/>
-        <input 
-        placeholder="Digite seu idade"
-        value={idade}
-        onChange={(e) => setIdade(e.target.value)}
-        
-        /><br/>
+      
         <button type='submit'>Registrar</button>
       </form>
       <br></br>
-      <div>
-        <span>Bem vindo: {user.nome} </span><br/>
-        <span>Idade: {user.idade}</span><br/>
-        <span>Email: {user.email}</span><br/>
-      </div>
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+  
+      </ul>
     
     
     </div>
