b8ff6ca0ce50fa65610c9ef85bddd38906261f07
Maysa Gonçalves <maysagoncalves2003@gmail.com>
2022-07-19 16:30:23 -0300

    ReactJs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+function App () {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState ('');
+  const [idade, setIdade] = useState(0);
+
+  const [user, setUser] = useState({});
+
+  function handleRegister(e) {
+    e.preventDefault();
+    alert('Usuário registrado com sucesso')
+    setUser({
+      nome: nome,
+      idade:idade,
+      email:email,
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Cadastrando usuário</h1>
+      <form onSubmit={handleRegister}>
+        <label>Nome: </label><br/>
+        <input 
+        placeholder="Digite seu nome"
+        value={nome} // valor dentro do input (já do html)
+        onChange={(e) => setNome(e.target.value)} //ele chama essa função toda vez que digitar qualquer letra. chama a funcao p/ pegar o que digitou  e coloca dnetro do useState
+        
+        /><br/>
+
+        <label>Email: </label><br/>
+        <input 
+        placeholder="Digite seu email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        /><br/>
+
+        <label>Idade: </label><br/>
+        <input 
+        placeholder="Digite seu idade"
+        value={idade}
+        onChange={(e) => setIdade(e.target.value)}
+        
+        /><br/>
+        <button type='submit'>Registrar</button>
+      </form>
+      <br></br>
+      <div>
+        <span>Bem vindo: {user.nome} </span><br/>
+        <span>Idade: {user.idade}</span><br/>
+        <span>Email: {user.email}</span><br/>
+      </div>
+    
+    
     </div>
-  );
+  )
 }
 
 export default App;
